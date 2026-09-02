64b7e1235f16bbd87c79801b5541252f0564d3c3
Le Vivilet <levivilet@pm.me>
2026-09-02 18:21:59 +0000

    test: use drop sessions in end to end tests

--- packages/e2e/src/viewlet.drag-and-drop-worker-uri-list.ts
+++ packages/e2e/src/viewlet.drag-and-drop-worker-uri-list.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.drag-and-drop-worker-uri-list'
 
-export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
+export const test: Test = async ({ Command, DragAndDrop, expect, FileSystem, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const first = `${tmpDir}/first-dropped.txt`
   const second = `${tmpDir}/second-dropped.txt`
@@ -12,13 +12,9 @@ export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, W
   ])
   await Workspace.setPath(tmpDir)
   await Main.closeAllEditors()
-  const itemId = await FileSystem.registerFileHandle({
-    kind: 'string',
-    type: 'text/uri-list',
-    value: `# ignored\r\n${first}\n${second}`,
-  } as any)
+  const dropId = await DragAndDrop.createDropSession([{ kind: 'string', type: 'text/uri-list', value: `# ignored\r\n${first}\n${second}` }])
 
-  await Command.execute('Main.handleDrop', [itemId])
+  await Command.execute('Main.handleDrop', dropId)
 
   const tabs = Locator('.MainTab')
   const firstTab = Locator('.MainTab[title$="first-dropped.txt"]')
